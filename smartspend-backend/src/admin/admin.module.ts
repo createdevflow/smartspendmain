8870f78f29d42eb3282cf2702aba51d89ecea61c
createdevflow <createdevflow@gmail.com>
2026-06-22 20:39:33 +0530

    fix(modules): remove redundant imports of global modules to prevent circular dependency build errors

--- smartspend-backend/src/admin/admin.module.ts
+++ smartspend-backend/src/admin/admin.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
-import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [MailModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
--- smartspend-backend/src/mail/mail.module.ts
+++ smartspend-backend/src/mail/mail.module.ts
@@ -1,10 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { PrismaModule } from '../prisma/prisma.module';
 
 @Global()
 @Module({
-  imports: [PrismaModule],
   providers: [MailService],
   exports: [MailService],
 })
